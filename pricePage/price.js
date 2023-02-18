0bf41e51db65ee73d1ce872a766a73dc4d505ba9
oryzaaa <oryzasativacikal@gmail.com>
2023-02-18 18:31:48 +0700

    ngerapihin

--- pricePage/price.js
+++ pricePage/price.js
@@ -1,9 +1,9 @@
 const buttonCart = document.querySelectorAll('.btn-primary')
 const valueCart = document.querySelector('.valueCart')
 
-buttonCart.forEach(el => {
+buttonCart.forEach(el => { //el merupakan sebuah parameter yang berfungsi untuk menampung  nilai atau variabel yang didefinisikan sebagai input atau argumen pada sebuah fungsi. Saat fungsi dipanggil, kita dapat memberikan nilai atau variabel sebagai argumen ke parameter fungsi tersebut
     el.addEventListener('click', () => {
-        let totalCart = Number(valueCart.innerText)
+        let totalCart = Number(valueCart.innerText)  //menggunakan properti "innerText" dan mengkonversinya ke tipe data numerik menggunakan fungsi "Number()". Kemudian, nilai tersebut ditampung dalam variabel "totalCart
         totalCart++
 
         valueCart.innerText = totalCart
@@ -12,7 +12,6 @@ buttonCart.forEach(el => {
 
 
 })
-
 
 const btnIcon = document.querySelector('.btnIcon')
 btnIcon.addEventListener('click', () => {
